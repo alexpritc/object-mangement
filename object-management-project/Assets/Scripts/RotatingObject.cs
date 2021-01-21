345ab20e44882cefb9963353cc039cfdb0609967
alexpritc <alexpritc@users.noreply.github.com>
2021-01-21 11:55:47 +0000

    Added level 4: rotating spawn areas

---
+++ object-management-project/Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : PersistableObject
+{
+    [SerializeField] private Vector3 angularVelocity;
+
+    private void Update()
+    {
+        transform.Rotate(angularVelocity * Time.deltaTime);
+    }
+}
